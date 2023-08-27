8e86b3f1edf9591f8432bba48f46c6b40a89b055
anirud <anirudngowda407@gmail.com>
2023-08-27 06:11:46 +0000

    jobby app almost completed

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,28 +1,25 @@
 import {Link} from 'react-router-dom'
+import Header from '../Header'
 import './index.css'
-import Navbar from '../Navbar/index'
 
 const Home = () => (
-  <>
-    <div className="home-cont">
-      <Navbar />
-      <div className="home-bottom-cont">
-        <div className="home-content-cont">
-          <h1 className="home-heading">Find The Job That Fits Your Life</h1>
-          <p className="home-para">
-            Millions of people are searching for jobs, salary
-            information,company reviews. Find the job that fits your abilities
-            and potential.
-          </p>
-          <Link to="/jobs">
-            <button className="find-jobs-btn" type="button">
-              Find Jobs
-            </button>
-          </Link>
-        </div>
+  <div className="home-container">
+    <Header />
+    <div className="home-page">
+      <div className="home-page-content">
+        <h1 className="home-heading">Find The Job That Fits Your Life</h1>
+        <p className="home-description">
+          Millions of people are searching for jobs, salary information, company
+          reviews. Find the job that fits your abilities and potential.
+        </p>
+        <Link to="/jobs">
+          <button className="find-jobs-button" type="button">
+            Find Jobs
+          </button>
+        </Link>
       </div>
     </div>
-  </>
+  </div>
 )
 
 export default Home
